c5e5c486bb511dfa9520c7efaf1a2814ef0ede36
HIRO15254 <hi089697@gmail.com>
2023-07-22 00:28:42 +0900

    ミドルウェアの設定

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
+function checkAuth(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1];
+    const [user, password] = atob(auth).split(':');
+
+    if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
+      return true;
+    }
+  }
+  return false;
+}
+
 export function middleware(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
+  if (process.env.VERCEL_ENV === 'preview') {
+    if (!checkAuth(req)) {
+      return new Response('Auth required', {
+        status: 401,
+        headers: {
+          'WWW-Authenticate': 'Basic realm="Secure Area"',
+        },
+      });
+    }
+  }
 
   const res = NextResponse.next({
     request: {
